6ce8e0e4cb08f9278b51c0774fde47f37e72b681
AdonayMorales29 <zadonay111@gmail.com>
2020-03-22 19:57:41 -0600

    actualizacion

---
+++ SRC.ts/app.ts
@@ -0,0 +1,6 @@
+import {CreateEpisodios} from "./Episodios"
+import {CreateStaff} from "./Staff"
+
+//CreateEpisodios("A","Direccion de A","1")
+
+CreateEpisodios("A","Mi super producto",20,30,100);
